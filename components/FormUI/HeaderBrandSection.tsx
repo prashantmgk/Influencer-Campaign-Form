3479671b666074fa34e1cca22c2394442a0d8995
prashantmgk <prashantmgk@gmail.com>
2024-06-19 19:39:28 +0545

    fixed the mobile responsive issue

--- components/FormUI/HeaderBrandSection.tsx
+++ components/FormUI/HeaderBrandSection.tsx
@@ -6,9 +6,9 @@ import MifuLogo from '../../images/mifu_logo.png';
 const HeaderBrandSection = () => {
    return (
       <Stack as='header' my={8} mx={{ lg: '8', base: '0' }} direction='row' justify="center" alignItems='center' spacing={4}>
-         <Image src={BreakAwayLogo.src} height={{ lg: "50px", base: "28px" }} alt='Breakaway Logo' />
+         <Image src={BreakAwayLogo.src} height={{ lg: "50px", base: "25px" }} alt='Breakaway Logo' />
          <Text fontSize='3xl' fontWeight='semibold'>x</Text>
-         <Box as='div' style={{ position: 'relative' }} width="200px" height={{ lg: "50px", base: "28px" }}>
+         <Box as='div' style={{ position: 'relative' }} width="200px" height={{ lg: "50px", base: "25px" }}>
             <Image src={MifuLogo.src} height="100%" minWidth='80px' alt='Mifu Logo' style={{ position: 'absolute' }} />
             <Image src={MifuLogo.src} height="100%" minWidth='80px' alt='Mifu Logo' opacity={0.3} style={{ position: 'absolute', top: '6px' }} />
          </Box>
